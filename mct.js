7d8e32859579e2e88bef8180237fe559dde5279b
saiteja <87071267+mukkasaiteja@users.noreply.github.com>
2022-11-17 08:39:39 +0530

    Add files via upload

---
+++ mct.js
@@ -0,0 +1,9 @@
+
+const container = document.querySelector(".main");
+const seats = document.querySelectorAll(".row .seat:not(.solded)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movielist");
+
+let ticketprice = movieSelect.Value;
+console.log(ticketprice);
